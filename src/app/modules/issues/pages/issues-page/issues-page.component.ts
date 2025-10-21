3edb974bba78c15390d7a50e6a40082de5620f85
JonathanYaguana <jonathansacrox93@gmail.com>
2025-10-21 15:53:26 -0500

    Componentes nuevos

---
+++ src/app/modules/issues/pages/issues-page/issues-page.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issues-page',
+  imports: [],
+  templateUrl: './issues-page.component.html',
+})
+export default class IssuesPageComponent { }
