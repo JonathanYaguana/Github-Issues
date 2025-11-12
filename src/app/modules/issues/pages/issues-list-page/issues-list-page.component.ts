3db0c95d96dd24ac577eb746bfbf7f3140d994e0
JonathanYaguana <jonathansacrox93@gmail.com>
2025-11-12 13:01:48 -0500

    fix(issues-list-page): se agrego una navegación para el issues-page

--- src/app/modules/issues/pages/issues-list-page/issues-list-page.component.ts
+++ src/app/modules/issues/pages/issues-list-page/issues-list-page.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-issues-list-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './issues-list-page.component.html',
 })
 export default class IssuesListPageComponent { }
